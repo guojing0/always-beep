bf206e235d51753cbf99bdc5db4367036ed25b7e
guoj <dev.guoj@gmail.com>
2013-11-22 21:44:25 +0800

    to space

--- beep.c
+++ beep.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-	while (1){
-		printf("\a");
-	}
-	return 0; /* It would never stop unless you press ctrl-c */
+    while (1){
+        printf("\a");
+    }
+    return 0; /* It would never stop unless you press ctrl-c */
 }
